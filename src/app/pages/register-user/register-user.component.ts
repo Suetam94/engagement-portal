cfe53b73c6ecf5c58d18cfdd06b3c874d8e06b17
Suetam94 <mateusviniciusdasilva@outlook.com>
2025-01-17 19:33:55 -0300

    feat: form validations

--- src/app/pages/register-user/register-user.component.ts
+++ src/app/pages/register-user/register-user.component.ts
@@ -1,19 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Title} from "@angular/platform-browser";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register-user',
   templateUrl: './register-user.component.html',
   styleUrls: ['./register-user.component.scss'],
 })
-export class RegisterUserComponent {
-  user = {
-    name: '',
-    email: '',
-    password: '',
-  };
+export class RegisterUserComponent implements OnInit {
+  registerForm!: FormGroup;
+  submitted = false;
 
-  constructor(private titleService: Title) {
+  constructor(private titleService: Title, private fb: FormBuilder) {
     this.updateTitle('Engagement Portal | Register User');
   }
 
@@ -22,6 +20,24 @@ export class RegisterUserComponent {
   }
 
   onSubmit(): void {
-    console.log('User Registered:', this.user);
+    this.submitted = true;
+
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    console.log('User Registered:', this.registerForm.value);
+  }
+
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  get f() {
+    return this.registerForm.controls;
   }
 }
